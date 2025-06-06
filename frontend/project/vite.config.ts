a7a0dd90f561e54a5871076852c9813ab28d7a33
Ubuntu <ubuntu@ip-172-31-40-110.us-east-2.compute.internal>
2025-06-06 13:24:09 +0000

    ajuste

--- frontend/project/vite.config.ts
+++ frontend/project/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// vite.config.ts
 export default defineConfig({
   server: {
     proxy: {
-      '/api': `http://18.221.67.44:3001/api`,
+      '/api': {
+        target: 'http://18.221.67.44:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix when forwarding
+      },
     },
   },
 });
